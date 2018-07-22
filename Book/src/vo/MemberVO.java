1ba655e490323467c95434098c3a58fcc4b77794
YOUNHEE <edam7635@gmail.com>
2018-07-22 17:24:25 +0900

    mingyu gu

--- Book/src/vo/MemberVO.java
+++ Book/src/vo/MemberVO.java
@@ -4,6 +4,7 @@ public class MemberVO {
 
 	public static void main(String[] args) {
 		int a = 3;
+		//mingyu gu wants to make conflicts on git so I make some possiblity
 		System.out.println(a);
 	}
 }
