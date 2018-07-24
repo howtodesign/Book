2d461855e8b7973c0b476733f06cfb6a6b6bc305
okok7028 <40908460+okok7028@users.noreply.github.com>
2018-07-24 14:35:19 +0900

    update MemberController

--- Book/Book/src/controller/MemberController.java
+++ Book/Book/src/controller/MemberController.java
@@ -18,11 +18,6 @@ public class MemberController {
 	@Autowired
 	private MemberService service;
 	
-	@RequestMapping("/joinForm.do") // go to joinPage
-	public String joinForm() {
-		return "join_form";
-	}
-	
 	@RequestMapping("/idCheck.do") // id check
 	@ResponseBody
 	public String idCheck(String id) {
@@ -52,24 +47,6 @@ public class MemberController {
 		}
 	}
 	
-	@RequestMapping("/login.do") // login
-	public ModelAndView loginProc(String userId, String userPw, HttpSession session) {
-		ModelAndView mv = new ModelAndView();
-		if(service.loginCheck(userId, userPw)) {
-			session.setAttribute("loginId", userId);
-			session.setAttribute("loginNick", service.getNickname(userId));
-			mv.setViewName("login_success");
-		}else {
-			mv.setViewName("login_fail");
-		}
-		return mv;
-	}
-	
-	@RequestMapping("/logout.do") // logout
-	public String logout(HttpSession session) {
-		session.invalidate();
-		return "logout";
-	}
 	
 	@RequestMapping("/memberInfo.do")
 	public ModelAndView memberInfo(String userPw, HttpSession session) {
@@ -109,5 +86,62 @@ public class MemberController {
 	    } catch (Exception e) {
 	        e.printStackTrace();
 	    }   
+	}
+	
+	@RequestMapping("/updateMember.do")
+	public ModelAndView updateProc(MemberVO member, HttpSession session) {
+		ModelAndView mv = new ModelAndView();
+			if(service.updateValidator(member)) {
+				mv.addObject("checkup", "success");
+				mv.setViewName("member_info");
+			}else {
+				mv.setViewName("update_fail");
+			}
+		return mv;		
+	}
+	
+	@RequestMapping("updatePasswordForm.do")
+	public String updatePasswordForm(){
+		return "update_password_form";
+	}
+	
+	@RequestMapping("updatePassword.do")
+	public String updatePassword(String beforepassword, String password, String passwordre, HttpSession session){
+		String id = (String)session.getAttribute("loginId");
+		if(service.loginCheck(id, beforepassword)){
+			if(password.equals(passwordre)){
+				if(service.updatePassword(id, password)>0){
+					session.invalidate();
+					return "update_success";
+				}else{
+					return "update_fail";
+				}
+			}else{
+				return "update_fail";
+			}
+		}else{
+			return "update_fail";
+		}
+	}
+	
+	@RequestMapping("deleteMemberForm.do")
+	public String deleteMemberForm(){
+		return "delete_member_form";
+	}
+	
+	@RequestMapping("deleteMember.do")
+	public String deleteMember(String password, String passwordre, HttpSession session){
+		String id = (String)session.getAttribute("loginId");
+		if(service.loginCheck(id, password)){
+			if(password.equals(passwordre)){
+				service.deleteMember(id);
+				session.invalidate();
+				return "delete_success";
+			}else{
+				return "delete_fail";
+			}
+		}else{
+			return "delete_fail";
+		}
 	}
 }
